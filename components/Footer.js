9279a2df0ea33d26031771ce1ffd67184b4c8d77
mohid <mohid10587@gmail.com>
2022-09-15 17:09:37 +0600

    ok

---
+++ components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div  style={{textAlign:'center',padding:'10px  0px' ,backgroundColor:'gray'}}>Footer</div>
+  )
+}
+
+export default Footer
